5d1889e5aa3079bfec591fdc38a04ab9e18bf9bf
peerax-PC\peerax <hemosiderin@gmail.com>
2016-10-30 11:59:33 +0700

    no message

--- src/bathtext.js
+++ src/bathtext.js
@@ -1,9 +1,22 @@
 var bathtext = function(value) {
     
-    
-  return isNumber(value);
+    return sepComma(value);
+  //return isNumber(value);
 };
 
+//Split comma
+function sepComma(n){
+    var parts = n.split(',');
+    var res;
+    parts.forEach(function(en) {
+        res += en;
+    });
+    
+    return res;
+}
+
+
+//Check numeric function
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
